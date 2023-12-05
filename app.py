5494192dc68be5d58d48016d5172d97cb7990b2b
Ramazan <121939721+ramazanabylkassov@users.noreply.github.com>
2023-12-05 15:41:32 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.enum.table import WD_ALIGN_VERTICAL
 from docx.shared import Inches
+import math
 # from streamlit_gsheets import GSheetsConnection
 
 def main():
@@ -15,20 +16,20 @@ def main():
     
     diseases_list = {
         'Pediatric Cardiology': {
-            'Common diseases': ['Tetralogy of Fallot', 'Pulmonary atresia', 'Transposition of Great Arteries', 'Single ventricle', 'AtrioVentricular septal defects', 'Atrial Septal Defect', 'Ventricular Septal Defect', 'Patent Ductus Arteriosus', 'Pulmonary stenosis', 'Coarctation of Aorta', 'Aortic Stenosis'],
-            'Frequency': (0.082608696, 0.069565217, 0.104347826, 0.086956522, 0.026086957, 0.369565217, 0.065217391, 0.139130435, 0.030434783, 0.008695652, 0.017391304),
+            'Common diseases': ['Aortic valve insufficiency', 'Atrial septal defect', 'Atrioventricular septal defect', 'Bicuspid aortic valve', 'Dilated cardiomyopathy', 'Familial Hypercholesterolemia', 'Familial hypercholesterolemia, heterozygous', 'Hypertrophic cardiomyopathy', 'Hypoplasia of right ventricle', 'Idiopathic pulmonary hypertension', 'Mitral valve regurgitation', 'Paroxysmal atrioventricular nodal reentry tachycardia', 'Pulmonary trunk atresia', 'Pulmonary valve regurgitation', 'Supraventricular tachycardia', 'Tetralogy of Fallot', 'Total anomalous venous drainage of pulmonary veins', 'Tricuspid valve regurgitation', 'Truncus arteriosus', 'VSD', 'Ventricular Septal Defect', 'WPW syndrome', 'Wolff-Parkinson-White syndrome (WPW)'],
+            'Frequency': [0.02631578947368421, 0.13157894736842105, 0.02631578947368421, 0.02631578947368421, 0.13157894736842105, 0.02631578947368421, 0.05263157894736842, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.05263157894736842, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.02631578947368421, 0.10526315789473684, 0.07894736842105263, 0.02631578947368421],
         },
         'Pediatric Gastroenterology': {
-            'Common diseases': ['Crohn disease', 'Ulcerative colitis', 'Celiac disease', 'Hepatitis of unknown origin'],
-            'Frequency': (0.49, 0.45, 0.05, 0.01),
+            'Common diseases': ["Crohn's Disease", 'Chronic hepatitis', 'Ulcerative Colitis', 'Protein–energy malnutrition', 'Behcet’s disease?', 'Celiac disease', 'Cystic Fibrosis', 'Autoimmune hepatitis', 'Dolichocolon', 'Hirshsprung disease', 'Short bowel syndrome ', 'Eosinophilic Esophagitis'],
+            'Frequency': (0.36585365853658536, 0.024390243902439025, 0.2926829268292683, 0.0975609756097561, 0.024390243902439025, 0.04878048780487805, 0.024390243902439025, 0.024390243902439025, 0.024390243902439025, 0.024390243902439025, 0.024390243902439025, 0.024390243902439025),
         },
         'Pediatric Rheumatology': {
-            'Common diseases': ['oligoarthritic JIA', 'polyarthritic JIA', 'enthesitis-related JIA', 'psoriatic JIA', 'SLE', 'dermatomyositis', 'scleroderma', 'vasculitis'],
-            'Frequency': (0.3, 0.2, 0.1, 0.05, 0.1, 0.1, 0.1, 0.05),
+            'Common diseases': ['Juvenile Dermatomyositis', 'JIA enthesitis associated', 'JIA oligoarticular', 'JIA polyarticular', 'JIA psoriatic', 'JIA systemic', 'Juvenile Scleroderma', 'Lupus nephritis', 'MAS', 'Reactive arthritis', 'Systemic lupus erythematosus'],
+            'Frequency': [0.125, 0.025, 0.25, 0.05, 0.025, 0.05, 0.2, 0.025, 0.025, 0.075, 0.15],
         },
         'Pediatric Endocrinology': {
-            'Common diseases': ['DMT1', 'DMT2', 'Precoutious puberty', 'Hypothyroidism', 'Hypogonadism', 'Turner syndrome', 'Androgen insensitivity syndrome'],
-            'Frequency': (0.45, 0.05, 0.1, 0.2, 0.1, 0.05, 0.05),
+            'Common diseases': ['Androgen insensitivity syndrome', 'Congenital Adrenal Hyperplasia', 'Diabetes mellitus, type 1', 'Diabetes mellitus, type 2', 'Diabetic ketoacidosis', 'Diabetic neuropathy', 'Growth delay', 'Hypergonadotropic hypogonadism', 'Morbid Obesity', 'Obesity stage 1', 'Osteogenesis imperfecta', 'Postoperative hypopituitarism', 'Rabson-Mendenhall syndrome', 'Short stature', 'Turner syndrome', 'X linked hereditary hypophosphatemia', 'precocious puberty'],
+            'Frequency': [0.029411764705882353, 0.058823529411764705, 0.35294117647058826, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.14705882352941177, 0.058823529411764705, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353],
         },
         'Pediatric Radiology': {
             'us': {
@@ -49,20 +50,20 @@ def main():
             },
         },
         'Pediatric Emergencies': {
-            'Common diseases': ['joint dislocation', 'Torus', 'Concussion', 'Green stick fracture', 'Cuts', 'Linear fracture', 'Spiral fracture'],
-            'Frequency': (0.2, 0.2, 0.1, 0.05, 0.1, 0.3, 0.05),
+            'Common diseases': ['Acute abdomen (gerd?)', 'Ankle sprain', 'Appendicitis', 'Burns involving less than 10% of body surface', 'Clavicle fracture', 'Constipation', 'Dislocation of the elbow', 'Fall from height, soft tissue bruises', 'Foreign body aspiration', 'Functional dyspepsia', 'GERD', 'Hand burn', 'Head concussions', 'Hypoglycemia', 'Infantile colic', 'Ingrown toenail', 'Intestinal obstruction', 'Intussusception', 'Leg fracture', 'Mesenteric lymphadenitis', 'Soft tissue bruises', 'UTI', 'Ulnar fracture', 'Wrist tendon injury'],
+            'Frequency': [0.03571428571428571, 0.07142857142857142, 0.07142857142857142, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.07142857142857142, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.03571428571428571, 0.07142857142857142, 0.03571428571428571, 0.03571428571428571],
         },
         'General Pediatrics and Pediatric Rehabilitation': {
             'Common diseases': ['cerebral palsy', 'autism', 'down syndrome'],
             'Frequency': (0.5, 0.3, 0.2),
         },
         'Pediatric Infectious Diseases': {
-            'Common diseases': ['URTI', 'Scarlet fever', 'hand foot mouth disease', 'measles', 'Rotavirus enteritis'],
-            'Frequency': (0.3, 0.1, 0.1, 0.3, 0.2),
+            'Common diseases': ['Acute Gastroenteritis', 'Acute Gastroenteritis Enterovirus', 'Acute bacterial tonsillitis', 'Acute bronchitis', 'Acute tonsillitis', 'Acute unilateral bacterial pneumonia of the right lower lobe', 'Bacterial meningitis', 'Croup', 'Measles', 'Pneumonia', 'Rhinopharyngitis', 'Scarlet fever', 'Shingles HZV infection', 'Staphylococcus infection', 'URTI', 'URTI with febrile seizures', 'VZV infection (chickenpox)', 'Viral meningitis'],
+            'Frequency': [0.14705882352941177, 0.029411764705882353, 0.029411764705882353, 0.08823529411764706, 0.08823529411764706, 0.029411764705882353, 0.08823529411764706, 0.058823529411764705, 0.058823529411764705, 0.058823529411764705, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.029411764705882353, 0.11764705882352941],
         },
         'Pediatric Oncology': {
-            'Common diseases': ['Nephroblastoma', 'Neuroblastoma', 'Astrocytoma', 'Medulloblastoma', 'Ependymoma'],
-            'Frequency': (0.3, 0.2, 0.3, 0.1, 0.1),
+            'Common diseases': ['Atypical teratoid rhabdoid tumor', 'Burkitt’s lymphoma', 'CNS tumour', 'Desmoplastic nodular medulloblastoma', 'Ganglioneuroblastoma', 'Hodgkin’s lymphoma', 'Medullary kidney carcinoma', 'Nephroblastoma recurrent', 'Nephroblastoma with mets', 'Neuroblastoma', 'Neuroblastoma recurrent', 'Neuroblastoma w/ mets', 'Osteogenic sarcoma', 'Retinoblastoma', 'Urinary bladder carcinoma'],
+            'Frequency': [0.09523809523809523, 0.047619047619047616, 0.09523809523809523, 0.047619047619047616, 0.09523809523809523, 0.047619047619047616, 0.047619047619047616, 0.047619047619047616, 0.047619047619047616, 0.09523809523809523, 0.047619047619047616, 0.047619047619047616, 0.14285714285714285, 0.047619047619047616, 0.047619047619047616],
         },
         'Pediatric Hematology': {
             'Common diseases': ['ALL', 'AML', 'CML', 'Lymphoma'],
@@ -76,21 +77,21 @@ def main():
             'Common diseases': ['Respiratory failure', 'Acute bronchiolitis', 'Acute respiratory infection', 'Postoperative complications', 'Hemolytic uremic syndrome', 'Pneumonia', 'Sepsis'],
             'Frequency': (0.15, 0.1, 0.2, 0.1, 0.05, 0.2, 0.2),
         },
-        'Neonatal Intensive Care Unit': {
-            'Common diseases': ['Meconium aspiration', 'Bradycardia', 'Bronchopulmonary dysplasia', 'Cerebral palsy', 'Cyanosis', 'Necrotizing enterocolitis', 'Congenital Heart Diseases', 'Pneumonia', 'Respiratory distress syndrome', 'Sepsis'],
-            'Frequency': (0.05, 0.05, 0.15, 0.05, 0.2, 0.05, 0.05, 0.1, 0.20, 0.1),
+        'Neonatology': {
+            'Common diseases': ['ABO Incompatibility', 'Anemia of a preterm baby', 'Asphyxia, cephalohematoma', 'Asphyxia, meconium aspiration', 'Aspiration pneumonia', 'Blood type Incompatibility', 'Bronchopulmonary Dysplasia', 'Cleft palate', 'Coarctation of aorta', 'Congenital anomaly of the urinary tract', 'Congenital pneumonia', 'Extremely low birth weight', 'Hydrocephalus', 'Hypoplasia of an aorta', 'Hypoxic-ischemic encephalopathy', 'Neonatal Jaundice', 'Neonatal Purpura Fulminans', 'Patent Ductus Arteriosus', 'Preterm baby', 'Preterm newborn', 'RDS', 'Respiratory Distress Syndrome', 'Subglottic stenosis', 'TORCH infection', 'Transient Tachypnea of the Newborn'],
+            'Frequency': [0.023809523809523808, 0.07142857142857142, 0.07142857142857142, 0.023809523809523808, 0.023809523809523808, 0.023809523809523808, 0.047619047619047616, 0.023809523809523808, 0.023809523809523808, 0.023809523809523808, 0.047619047619047616, 0.023809523809523808, 0.023809523809523808, 0.023809523809523808, 0.07142857142857142, 0.14285714285714285, 0.023809523809523808, 0.023809523809523808, 0.023809523809523808, 0.023809523809523808, 0.07142857142857142, 0.047619047619047616, 0.023809523809523808, 0.023809523809523808, 0.047619047619047616],
         },
         'Pediatric Allergology, Immunology and Pulmonology': {
-            'Common diseases': ['Common variable immunodeficiency', 'Selective IgA deficiency', 'Chronic Granulomatous Disease', 'Allergic rhinitis', 'Asthma', 'Immunodeficiency', 'Contact dermatitis'],
-            'Frequency': (0.05, 0.15, 0.05, 0.3, 0.3, 0.1, 0.05),
+            'Common diseases': ['Allegic rhinitis', 'Allergy', 'Atopic dermatitis', 'Bronchial asthma', 'Immune deficiency ?', 'Immunodeficiency? CMV infection', 'Primary immune deficiency', 'Rickets', 'Urticaria'],
+            'Frequency': [0.05555555555555555, 0.35185185185185186, 0.018518518518518517, 0.2037037037037037, 0.09259259259259259, 0.018518518518518517, 0.07407407407407407, 0.018518518518518517, 0.16666666666666666],
         },
-        'Neonatology': {
-            'Common diseases': ['Neonatal jaundice', 'Pneumonia', 'Tetrado Fallo', 'AVSD', 'preterm', 'BPD', 'RDS'],
-            'Frequency': (0.3, 0.1, 0.05, 0.05, 0.2, 0.2, 0.1),
+        'Neonatal Intensive Care Unit': {
+            'Common diseases': ['Asphyxia', 'Aspiration pneumonia', 'Blood type Incompatibility', 'Bronchopulmonary Dysplasia', 'Coarctation of aorta', 'Diaphragmatic hernia', 'Gastroschisis', 'Hemolytic disease', 'Hirschsprung disease', 'Intrauterine Growth Restriction', 'Intrauterine growth retardation ', 'Meconium aspiration ', 'NEC', 'Necrotizing enterocolitis', 'Pyloric stenosis', 'Respiratory Distress Syndrome', 'Retinopathy of prematurity', 'Sepsis'],
+            'Frequency': [0.02564102564102564, 0.05128205128205128, 0.05128205128205128, 0.1282051282051282, 0.02564102564102564, 0.05128205128205128, 0.05128205128205128, 0.05128205128205128, 0.02564102564102564, 0.05128205128205128, 0.02564102564102564, 0.02564102564102564, 0.05128205128205128, 0.02564102564102564, 0.05128205128205128, 0.10256410256410256, 0.15384615384615385, 0.05128205128205128],
         },
         'Pediatric Neurology': {
-            'Common diseases': ['Autism spectrum disorder', 'Cerebral palsy', 'Epilepsy', 'Subacute Sclerosing Panenecephalitis', "Rasmussen's encephalitis", 'Duschenne muscular dystrophy'],
-            'Frequency': (0.4, 0.1, 0.3, 0.1, 0.05, 0.05),
+            'Common diseases': ['Autistic Spectrum Disorder', 'Acute viral encephalitis', 'Cerebral Palsy', 'Focal seizures', 'Generalized Epilepsy', 'Gullain Barre Syndrome', 'Meningocele spina bifida', 'Metabolic disorder?', 'Muscular Dystrophies', 'Myotonic Dystrophy', 'Neurofibromatosis type I', 'Psychomotor delay', 'Speech delay', 'Spinal Muscular Atrophy', 'Subacute sclerosing panencephalitis', 'Tourette syndrome', "Wilson's disease"],
+            'Frequency': [0.15384615384615385, 0.02564102564102564, 0.1282051282051282, 0.05128205128205128, 0.20512820512820512, 0.02564102564102564, 0.02564102564102564, 0.05128205128205128, 0.02564102564102564, 0.02564102564102564, 0.05128205128205128, 0.07692307692307693, 0.02564102564102564, 0.02564102564102564, 0.05128205128205128, 0.02564102564102564, 0.02564102564102564],
         },
     }
     
@@ -114,13 +115,13 @@ def main():
         }
 
     with st.sidebar:
-        name = st.text_input('Enter your first & last name:', value='Dimitri Poddighe')
+        name = st.text_input('Enter your first & last name:', value='Student Studentov')
         department = st.selectbox('Choose department:', sorted(departments.keys(), key=str.lower))
         tutor = st.text_input("Enter clinical preceptor's name:", value=departments[department]['tutor'])
         hospital = st.selectbox("Choose hospital:", hospitals, index=hospitals.index(departments[department]['hospital']))
         cols = st.columns(2)
-        start_date = cols[0].date_input('Enter start date')
-        end_date = cols[1].date_input('Enter end date', value=start_date+timedelta(days=54))
+        start_date = cols[0].date_input('Enter start date', format="DD.MM.YYYY")
+        end_date = cols[1].date_input('Enter end date', value=start_date+timedelta(days=53), format="DD.MM.YYYY")
         
         st.write("Select the amount of patients:")
         if department == 'Pediatric Radiology':
@@ -143,7 +144,10 @@ def main():
                 preferent_age_group = st.select_slider('Preferentiable age range (years)', options=np.arange(0, 18, step=0.5), value=(0, 17.5))
                 preferent_probab = st.select_slider('Percent of patients in the selected age group (%)', options=np.arange(0, 101, step=1), value=50)
                 remaining_step = (((preferent_age_group[0]-0.1) + (17.9 - preferent_age_group[1]))) / (patient_amount*(100-preferent_probab)/100)
-                age_range = np.concatenate([np.linspace(preferent_age_group[0], preferent_age_group[1], num=round(patient_amount*preferent_probab/100)), np.arange(0.1, preferent_age_group[0], step=remaining_step), np.arange(preferent_age_group[1], 17.9, step=remaining_step)])
+                if preferent_probab == 100:
+                    age_range = np.linspace(preferent_age_group[0], preferent_age_group[1], num=patient_amount)
+                else:
+                    age_range = np.concatenate([np.linspace(preferent_age_group[0], preferent_age_group[1], num=round(patient_amount*preferent_probab/100)), np.arange(0.1, preferent_age_group[0], step=remaining_step), np.arange(preferent_age_group[1], 17.9, step=remaining_step)])
 
 
     # st.dataframe(diseases_list[department])
@@ -183,11 +187,11 @@ def main():
     patients_age = np.empty_like(patients_age_numeric, dtype='object')
     for index, age in enumerate(patients_age_numeric):
         if department in ['Neonatology', 'Neonatal Intensive Care Unit']:
-            patients_age[index] = f'{round(age)} days'
+            patients_age[index] = f'{math.floor(age)} days'
         elif age >= 1:
-            patients_age[index] = f'{round(age)} {"years" if age > 1 else "year"}'
+            patients_age[index] = f'{math.floor(age)} {"years" if age > 1 else "year"}'
         else:
-            patients_age[index] = f'{round(age*12)} months'
+            patients_age[index] = f'{math.ceil(age*12)} months'
 
     patients_gender = np.random.choice(['male', 'female'], size=patient_amount)
     
@@ -247,24 +251,30 @@ def main():
 
     resident = doc.add_paragraph()
     resident.add_run('RESIDENT: ').bold = True
+    resident.italic = True
     resident.add_run(f'{name}')
     rotation = doc.add_paragraph()
     rotation.add_run('ROTATION: ').bold = True
+    rotation.italic = True
     rotation.add_run(f'{department}')
     hospital_site = doc.add_paragraph()
     hospital_site.add_run('HOSPITAL SITE: ').bold = True
+    hospital_site.italic = True
     hospital_site.add_run(f'{hospital}')
     attendance = doc.add_paragraph()
     attendance.add_run('ATTENDANCE DATE: ').bold = True
+    attendance.italic = True
     attendance.add_run('From: ').bold = True
     attendance.add_run(f'{start_date} ')
     attendance.add_run('To: ').bold = True
     attendance.add_run(f'{end_date}')
     supervisor = doc.add_paragraph()
     supervisor.add_run('SUPERVISOR (name and surname): ').bold = True
+    supervisor.italic = True
     supervisor.add_run(f'{tutor}')
     signature  = doc.add_paragraph()
     signature.add_run('SUPERVISOR (signature): ').bold = True
+    signature.italic = True
 
     # Initialise the table
     t = doc.add_table(rows=(df_output.shape[0] + 1), cols=df_output.shape[1])
